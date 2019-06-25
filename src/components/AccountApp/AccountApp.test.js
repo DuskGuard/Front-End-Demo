aba31e7cfbe1af4e90edc530505422f4059a8fb6
Trent Apt <48695011+DuskGuard@users.noreply.github.com>
2019-06-25 08:53:52 -0700

    Fixed some Testing Issues

--- src/components/AccountApp/AccountApp.test.js
+++ src/components/AccountApp/AccountApp.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AccountApp from './AccountApp';
+import AccountPOJO from './AccountPOJO'
 
+let detailTargetAccount = new AccountPOJO(500,"Trent")
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<AccountApp />, div);
+  ReactDOM.render(<AccountApp targetAccount={detailTargetAccount} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
